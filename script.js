582e54f8dd0417f2f2594562d59fc8bf4d272d9d
alexandrebelem21 <alexandrebelem21@gmail.com>
2022-08-11 17:49:57 -0300

    Requisitos Obrigatorios

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let email = document.querySelector("#email");
 let pass = document.querySelector("#pass");
+let btn = document.querySelector("#submit-btn");
+let agree = document.querySelector("#agreement");
 
 function verificaSenha() {
   if (email.value === "tryber@teste.com" && pass.value === "123456") {
     alert("Olá, Tryber!");
   } else {
     alert("Email ou senha inválidos.");
+  }
+}
+
+function ativar() {
+  if (agree.checked) {
+    btn.removeAttribute("disabled");
+  } else {
+    btn.setAttribute("disabled", true);
   }
 }
